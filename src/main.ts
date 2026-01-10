432c65fa04564b4f02b8de295a4a4c07fc7f17df
82PareshPatil <paresh2004patil@gmail.com>
2026-01-10 12:38:47 +0530

    profile table create and a new file Users setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 1. Create Swagger Config
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Blog Backend')
+    .setDescription('The DevBlog API description')
+    .setVersion('1.0')
+    .build();
+
+  // 2. Setup Swagger Module
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Access at localhost:3000/api
+
+  await app.listen(3000);
 }
 bootstrap();
